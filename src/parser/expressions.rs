d7e055c293fb2297c44950ac952f67696205431b
Tyler Calder <tyler.calder@advancedmd.com>
2023-05-25 09:45:07 -0600

    refactor: Move Primary Expressions to new module

--- src/parser/expressions.rs
+++ src/parser/expressions.rs
@@ -1,4 +1,6 @@
+mod primary_expressions;
 use crate::ParseError;
+use primary_expressions::PrimaryExpression;
 
 use super::{
     identifier::Identifier,
@@ -81,155 +83,13 @@ impl<'a> VariableAssignment<'a> {
     }
 }
 
-// primary-expression:
-// x      literal-expression
-// x      list-expression
-//       record-expression
-// x       identifier-expression
-//       section-access-expression
-//       parenthesized-expression
-//       field-access-expression
-//       item-access-expression
-// x      invoke-expression
-//       not-implemented-expression
-//
-/// For now We are only implementing a parser for Identifier Expression and
-/// Invoke Expressions. This is the minimum we need for the task.
-#[derive(Debug)]
-enum PrimaryExpression<'a> {
-    /// invoke-expression:
-    ///     primary-expression <(> argument-listopt <)>
-    /// argument-list:
-    ///     expression
-    ///     expression , argument-list
-    List(ListExpression<'a>),
-    Identifier(Identifier<'a>),
-    Invoke(Box<Invocation<'a>>),
-    Literal(Literal<'a>),
-}
-
-impl<'a> PrimaryExpression<'a> {
-    // parses the stuff and returns an instance of itself
-    fn try_parse(text: &'a str) -> Result<(usize, Self), ParseError> {
-        if let Ok((i, val)) = Literal::try_parse(text) {
-            return Ok((i, PrimaryExpression::Literal(val)));
-        }
-        if let Ok((i, val)) = Invocation::try_parse(text) {
-            return Ok((i, PrimaryExpression::Invoke(Box::new(val))));
-        }
-        if let Ok((i, val)) = ListExpression::try_parse(text) {
-            return Ok((i, PrimaryExpression::List(val)));
-        }
-        if let Ok((i, val)) = Identifier::try_parse(text) {
-            return Ok((i, PrimaryExpression::Identifier(val)));
-        }
-        Err(ParseError::InvalidInput {
-            pointer: 0,
-            ctx: parse_utils::gen_error_ctx(text, 0, 5),
-        })
-    }
-}
-
-#[derive(Debug)]
-struct ListExpression<'a> {
-    elements: Vec<PrimaryExpression<'a>>,
-}
-
-impl<'a> ListExpression<'a> {
-    pub fn try_parse(text: &'a str) -> ParseResult<Self> {
-        let mut parse_pointer = skip_whitespace(text);
-
-        if text[parse_pointer..].chars().next().unwrap_or(' ') != '{' {
-            return Err(Box::new(ParseError::InvalidInput {
-                pointer: parse_pointer,
-                ctx: gen_error_ctx(text, parse_pointer, 5),
-            }));
-        }
-        parse_pointer += 1;
-        let mut elements = vec![];
-        loop {
-            let (delta, el) = PrimaryExpression::try_parse(&text[parse_pointer..])?;
-            parse_pointer += delta + skip_whitespace(&text[parse_pointer + delta..]);
-            elements.push(el);
-
-            if text[parse_pointer..].chars().next().unwrap_or('}') == '}' {
-                parse_pointer += 1; // Add to account that we have moved one forward
-                break;
-            }
-            if text[parse_pointer..].chars().next().unwrap_or(',') != ',' {
-                panic!("This is unexpected");
-            }
-            parse_pointer += 1; // Skip the comma
-        }
-
-        Ok((parse_pointer, Self { elements }))
-    }
-}
-
-#[derive(Debug)]
-struct Invocation<'a> {
-    pub invoker: PrimaryExpression<'a>,
-    pub args: Vec<PrimaryExpression<'a>>,
-}
-
-impl<'a> Invocation<'a> {
-    #[cfg(test)]
-    fn new(invoker: PrimaryExpression<'a>, args: Vec<PrimaryExpression<'a>>) -> Self {
-        Self { invoker, args }
-    }
-
-    pub fn try_parse(text: &'a str) -> ParseResult<Invocation<'a>> {
-        // To start, we need to identifiy the calling Expresion. Lets try:
-        let mut parse_pointer = 0;
-        let (delta, invoker) = Identifier::try_parse(text)?;
-
-        parse_pointer += delta;
-        let mut args = vec![];
-        parse_pointer += skip_whitespace(&text[parse_pointer..]);
-        if !&text[parse_pointer..].starts_with('(') {
-            // It is invalid for a invokation to not start with '('
-            return Err(Box::new(ParseError::InvalidInput {
-                pointer: parse_pointer,
-                ctx: parse_utils::gen_error_ctx(text, parse_pointer, 5),
-            }));
-        } else {
-            parse_pointer += 1; // Skip the opening '('
-        }
-        // Now we need to Parse the contents of the function invocation
-        loop {
-            let (delta, arg) = PrimaryExpression::try_parse(&text[parse_pointer..])?;
-            args.push(arg);
-            parse_pointer = parse_pointer
-                + delta
-                + parse_utils::skip_whitespace(&text[parse_pointer + delta..]);
-            // If we come to the end of the text of the invocation, we want
-            // to end
-            if text[parse_pointer..].chars().next().unwrap_or(')') == ')' {
-                parse_pointer += 1; // Add to account that we have moved one forward
-                break;
-            }
-            if text[parse_pointer..].chars().next().unwrap_or(',') != ',' {
-                panic!("This is unexpected");
-            }
-            parse_pointer += 1; // Skip the comma
-        }
-
-        Ok((
-            parse_pointer,
-            Invocation {
-                invoker: PrimaryExpression::Identifier(invoker),
-                args,
-            },
-        ))
-    }
-}
 
 #[cfg(test)]
 mod tests {
     use std::{assert_eq, todo};
 
     use super::*;
-    use crate::parser::literal::NumberType;
+    use crate::parser::{identifier::Identifier, literal::NumberType};
     use assert_matches::assert_matches;
     use rstest::rstest;
 
@@ -286,7 +146,7 @@ var = "Not a variable""#,
     #[case(
     r#"       var =          This("Not a variable")"#,
     "var",
-    PrimaryExpression::Invoke(Box::new(Invocation::new(
+    PrimaryExpression::Invoke(Box::new(primary_expressions::Invocation::new(
         PrimaryExpression::Identifier(Identifier::new("This")),
         vec![PrimaryExpression::Literal(Literal::Text("Not a variable"))]
     ))),
@@ -295,7 +155,7 @@ var = "Not a variable""#,
     #[case(
     r#"       #"var" =          This.that("Not a variable")"#,
     "var",
-    PrimaryExpression::Invoke(Box::new(Invocation::new(
+    PrimaryExpression::Invoke(Box::new(primary_expressions::Invocation::new(
         PrimaryExpression::Identifier(Identifier::new("This.that")),
         vec![PrimaryExpression::Literal(Literal::Text("Not a variable"))]
     ))),
@@ -316,139 +176,4 @@ var = "Not a variable""#,
         assert_matches!(assignment.expr, _expr)
     }
 
-    #[rstest]
-    #[case(r#"This("Not a variable", "More Text")"#, "This", vec!["Not a variable", "More Text"])]
-    #[case(r#"This("Not a variable")"#, "This", vec!["Not a variable"])]
-    fn test_invokation_parser(
-        #[case] input_text: &str,
-        #[case] ident: &str,
-        #[case] vars: Vec<&str>,
-    ) {
-        let (_, invokation) = Invocation::try_parse(input_text)
-            .expect(format!("failed to parse test input '{}'", &input_text).as_str());
-
-        if let PrimaryExpression::Identifier(invoker) = invokation.invoker {
-            assert_eq!(invoker.text(), ident)
-        }
-
-        assert_eq!(invokation.args.len(), vars.len());
-        for (i, arg) in invokation.args.iter().enumerate() {
-            match arg {
-                PrimaryExpression::Literal(Literal::Text(v)) => assert_eq!(v, &vars[i]),
-                _ => assert!(false),
-            }
-        }
-    }
-
-    #[rstest]
-    #[case(
-    r#"This("Not a variable", true, identifier)"#, 
-    "This",
-    vec![
-         PrimaryExpression::Literal(Literal::Text("Not a variable")), 
-         PrimaryExpression::Literal(Literal::Logical(true)),
-         PrimaryExpression::Identifier(Identifier::new("identifier"),
-    )],
-    40
-) ]
-    #[case(
-    r#"This(#!" Not a 235.E10 variable"  ,false  , 1234.5 , 0x25)"#, 
-    "This", 
-    vec![
-        PrimaryExpression::Literal(Literal::Verbatim(" Not a 235.E10 variable")), 
-        PrimaryExpression::Literal(Literal::Logical(false)),
-        PrimaryExpression::Literal(Literal::Number(NumberType::Float(1234.5))),
-        PrimaryExpression::Literal(Literal::Number(NumberType::Int(0x25))),
-    ],
-58) ]
-    #[case(
-    r#"This(" Not a 235.E10 variable", false, 1234.5, 0x25)"#, 
-    "This", 
-    vec![
-        PrimaryExpression::Literal(Literal::Text(" Not a 235.E10 variable")), 
-        PrimaryExpression::Literal(Literal::Logical(false)),
-        PrimaryExpression::Literal(Literal::Number(NumberType::Float(1234.5))),
-        PrimaryExpression::Literal(Literal::Number(NumberType::Int(0x25))),
-    ],
-52)
-]
-    fn test_invokation_parser_mixed(
-        #[case] input_text: &str,
-        #[case] ident: &str,
-        #[case] vars: Vec<PrimaryExpression>,
-        #[case] exp_delta: usize,
-    ) {
-        let (delta, invokation) = Invocation::try_parse(input_text)
-            .expect(format!("failed to parse test input '{}'", &input_text).as_str());
-
-        if let PrimaryExpression::Identifier(invoker) = invokation.invoker {
-            assert_eq!(invoker.text(), ident)
-        }
-        assert_eq!(exp_delta, delta);
-
-        for (i, arg) in invokation.args.iter().enumerate() {
-            match arg {
-                PrimaryExpression::List(_) => todo!(),
-                PrimaryExpression::Identifier(ident) => {
-                    assert_matches!(&vars[i], PrimaryExpression::Identifier(expected) => {
-                        assert_eq!(ident.text(), expected.text())
-                    })
-                }
-                PrimaryExpression::Invoke(_invoke) => todo!(),
-                PrimaryExpression::Literal(_literal) => {
-                    assert_matches!(&vars[i], PrimaryExpression::Literal(expected) => {
-                        assert_matches!(expected, _literal)
-                    })
-                }
-            }
-        }
-    }
-
-    #[rstest]
-    #[case("identifier")]
-    fn test_invocation_parser_fail(#[case] input_text: &str) {
-        match Invocation::try_parse(input_text) {
-            Err(_) => assert!(true),
-            Ok(_) => assert!(false),
-        }
-    }
-
-    #[rstest]
-    #[case(
-    r#"{" Not a 235.E10 variable", false, 1234.5, 0x25}"#, 
-    vec![
-        PrimaryExpression::Literal(Literal::Text(" Not a 235.E10 variable")), 
-        PrimaryExpression::Literal(Literal::Logical(false)),
-        PrimaryExpression::Literal(Literal::Number(NumberType::Float(1234.5))),
-        PrimaryExpression::Literal(Literal::Number(NumberType::Int(0x25))),
-    ],
-48)
-]
-    fn test_list_expression_parser(
-        #[case] input_text: &str,
-        #[case] exp_elements: Vec<PrimaryExpression>,
-        #[case] exp_delta: usize,
-    ) {
-        let (delta, list) = ListExpression::try_parse(input_text)
-            .expect(format!("failed to parse test input '{}'", &input_text).as_str());
-
-        assert_eq!(exp_delta, delta);
-
-        for (i, arg) in list.elements.iter().enumerate() {
-            match arg {
-                PrimaryExpression::List(_) => todo!(),
-                PrimaryExpression::Identifier(ident) => {
-                    assert_matches!(&exp_elements[i], PrimaryExpression::Identifier(expected) => {
-                        assert_eq!(ident.text(), expected.text())
-                    })
-                }
-                PrimaryExpression::Invoke(_invoke) => todo!(),
-                PrimaryExpression::Literal(_literal) => {
-                    assert_matches!(&exp_elements[i], PrimaryExpression::Literal(expected) => {
-                        assert_matches!(expected, _literal)
-                    })
-                }
-            }
-        }
-    }
 }
