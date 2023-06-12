dcf6345877e04125c1cf806c363e29ce9f057ddd
Tyler Calder <tyler.calder@advancedmd.com>
2023-06-12 15:16:56 -0600

    feat: Introduce core TryParse trait

---
+++ src/parser/core.rs
@@ -0,0 +1,8 @@
+use super::parse_utils::ParseResult;
+
+/// Core utilities for parser
+
+pub trait TryParse<'a> {
+    /// Try to parse the struct from the text
+    fn try_parse(text: &'a str) -> ParseResult<Self> where Self: Sized;
+}
