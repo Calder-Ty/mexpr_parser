fcc0a8f14906d214faa0032cb529130cda8c2d0d
Tyler Calder <tyler.calder@advancedmd.com>
2023-06-16 11:16:55 -0600

    feat: Change Field access to parse open expression
    Like ItemAccess, FieldAccess will now contain its initial Primary
    Expression, IF it exists.

--- src/parser/expressions/primary_expressions.rs
+++ src/parser/expressions/primary_expressions.rs
@@ -41,7 +41,7 @@ pub(crate) enum PrimaryExpression<'a> {
     Invoke(Box<Invocation<'a>>),
     Literal(Literal<'a>),
     Record(Record<'a>),
-    FieldAccess(FieldAccess<'a>),
+    FieldAccess(Box<FieldAccess<'a>>),
     ItemAccess(Box<ItemAccess<'a>>),
 }
 
@@ -67,7 +67,7 @@ impl<'a> PrimaryExpression<'a> {
             return Ok((i, PrimaryExpression::Identifier(val)));
         }
         if let Ok((i, val)) = FieldAccess::try_parse(text) {
-            return Ok((i, PrimaryExpression::FieldAccess(val)));
+            return Ok((i, PrimaryExpression::FieldAccess(Box::new(val))));
         }
         Err(ParseError::InvalidInput {
             pointer: 0,
@@ -215,14 +215,10 @@ fn arg_lookahead(text: &str) -> bool {
 #[derive(Debug, PartialEq, Serialize)]
 pub(crate) struct FieldAccess<'a> {
     // The Grammar posted by Microsoft for Field Access Expressions has it start with
-    // a Primary Expression. However, this recursive relationship is dangerous, as there
-    // is a very legitimate opportunity that we would be stuck in a loop because the pointer will
-    // never progress, and keep trying to find an expression.
-    //
-    // Since FieldAccess expressions can be done alone (something mentioned in the docs, but
-    // not allowed by the Grammar). We are going to just assume that a Primary Expression preceeds
-    // field Access, because it won't always be the case anyway.
+    // a Primary Expression. This is required by the spec, but in practice
+    // it doesn't always have to be there. (eg, each statement).
     // https://learn.microsoft.com/en-us/powerquery-m/m-spec-functions#simplified-declarations
+    expr: Option<PrimaryExpression<'a>>,
     selector: Identifier<'a>,
 }
 
@@ -232,6 +228,32 @@ impl<'a> TryParse<'a> for FieldAccess<'a> {
         Self: Sized,
     {
         let mut parse_pointer = skip_whitespace(text);
+
+        // For FieldAccess to be valid, There _has_ to be an '[' somewhere near.
+        // To ensure that we are not in a situation where we recurse forever
+        // and overflow the stack, we are going to make sure that the primary Expression we
+        // parse is limited to the text immediately preceding the next `[`
+        //
+        // If There is no `[`, we will fail, as the current expression we are parsing cannot
+        // possibly be an FieldAccess.
+        if !(&text[parse_pointer..].contains(operators::OPEN_BRACKET_STR)) {
+            return Err(Box::new(ParseError::InvalidInput {
+                pointer: parse_pointer,
+                ctx: gen_error_ctx(text, parse_pointer, 5),
+            }));
+        }
+
+        let primary_end = text
+            .chars()
+            .take_while(|c| *c != operators::OPEN_BRACKET)
+            .count();
+
+        let (delta, expr) = match PrimaryExpression::try_parse(&text[parse_pointer..primary_end]) {
+            Ok((i, v)) => (i, Some(v)),
+            Err(_) => (0, None),
+        };
+
+        parse_pointer += delta;
 
         if !(next_char(&text[parse_pointer..]).unwrap_or(' ') == operators::OPEN_BRACKET) {
             return Err(Box::new(ParseError::InvalidInput {
@@ -255,30 +277,26 @@ impl<'a> TryParse<'a> for FieldAccess<'a> {
 
         parse_pointer += 1;
 
-        Ok((parse_pointer, Self { selector }))
+        Ok((parse_pointer, Self { expr, selector }))
     }
 }
 
-
 #[derive(Debug, PartialEq, Serialize)]
 pub(crate) struct ItemAccess<'a> {
-
     expr: PrimaryExpression<'a>,
-    selector: Expression<'a>
-
+    selector: Expression<'a>,
 }
 
 impl<'a> TryParse<'a> for ItemAccess<'a> {
-
     fn try_parse(text: &'a str) -> ParseResult<Self>
-        where
-            Self: Sized {
-
+    where
+        Self: Sized,
+    {
         let mut parse_pointer = skip_whitespace(text);
 
         // For Item Access to be valid, There _has_ to be an '{' somewhere near.
         // To ensure that we are not in a situation where we recurse forever
-        // and overflow the stack, we are going to make sure that the primary Expression we 
+        // and overflow the stack, we are going to make sure that the primary Expression we
         // parse is limited to the text immediately preceding the next `{`
         //
         // If There is no `{`, we will fail, as the current expression we are parsing cannot
@@ -289,7 +307,10 @@ impl<'a> TryParse<'a> for ItemAccess<'a> {
                 ctx: gen_error_ctx(text, parse_pointer, 5),
             }));
         }
-        let primary_end = text.chars().take_while(|c| *c != operators::OPEN_BRACE).count();
+        let primary_end = text
+            .chars()
+            .take_while(|c| *c != operators::OPEN_BRACE)
+            .count();
         let (delta, expr) = PrimaryExpression::try_parse(&text[parse_pointer..primary_end])?;
         parse_pointer += delta;
         parse_pointer += skip_whitespace(&text[parse_pointer..]);
@@ -301,22 +322,29 @@ impl<'a> TryParse<'a> for ItemAccess<'a> {
             }));
         };
         parse_pointer += 1; // Advance past the `{`
-        //
+                            //
         let (delta, selector) = Expression::try_parse(&text[parse_pointer..])?;
         parse_pointer += delta;
         let lookahead_pointer = skip_whitespace(&text[parse_pointer..]);
 
-        if !(next_char(&text[parse_pointer + lookahead_pointer..]).unwrap_or(' ') == operators::CLOSE_BRACE) {
+        if !(next_char(&text[parse_pointer + lookahead_pointer..]).unwrap_or(' ')
+            == operators::CLOSE_BRACE)
+        {
             return Err(Box::new(ParseError::InvalidInput {
                 pointer: parse_pointer,
                 ctx: gen_error_ctx(text, parse_pointer, 5),
             }));
         };
         parse_pointer += lookahead_pointer + 1; // Advance past the `}`
-        Ok((parse_pointer, Self { expr: expr, selector }))
+        Ok((
+            parse_pointer,
+            Self {
+                expr: expr,
+                selector,
+            },
+        ))
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -490,7 +518,7 @@ mod tests {
     #[case(
         r#" [  text ]"#, 
         10,
-        FieldAccess { selector: Identifier::new("text") } 
+        FieldAccess { expr: None, selector: Identifier::new("text") }
     )
     ]
     fn test_field_access_expression(
@@ -538,13 +566,9 @@ mod tests {
         assert_eq!(exp_field, field);
     }
 
-
     #[rstest]
-    #[case(r#" { value }"#,)
-    ]
-    fn test_item_access_fails(
-        #[case] input_text: &str,
-    ) {
+    #[case(r#" { value }"#)]
+    fn test_item_access_fails(#[case] input_text: &str) {
         let res = ItemAccess::try_parse(input_text);
         match res {
             Ok(_) => assert!(false),
